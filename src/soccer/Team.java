bf425ab986ccc06924571d7ebb2804ee07b4f49f
Jose Manuel Mendez <jmanuel34@gmail.com>
2021-01-26 13:50:12 +0100

    Activity Guide 6.2

---
+++ src/soccer/Team.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author josemanuelmendez
+ */
+package soccer;
+        
+public class Team {
+    public String teamName;
+    public Player[] playerArray;
+}
